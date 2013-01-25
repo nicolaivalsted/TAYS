537425ceb040f6159d0badae483dc2dd2358f859
Anders Kristian Andersen <aka@myself.com>
2013-01-25 16:52:41 +0100

    TAYS-1297 cleaned up in service OrderDataType before introducing nick names

--- smp-client/src/main/java/dk/yousee/smp/order/model/Constants.java
+++ smp-client/src/main/java/dk/yousee/smp/order/model/Constants.java
@@ -10,16 +10,6 @@ package dk.yousee.smp.order.model;
 public class Constants {
 
 
-//    public static final String STATUS_CODE_ASYNC_STARTED = "ASYNC_STARTED";
-
-
-//    public static final OrderDataType FEATURE_LIST_ADDRESS = new OrderDataType( "FEATURE_ADDRESS");
-//    public static final OrderDataType ORDER_STATE = new OrderDataType( ServicePrefix.smp,"SubOrderStateType");
-
-//    /* Magiske vaerdier til at genkende parameter vaerdier og navne, saa de kan blive oversat til sigma-sprog */
-//    public static final OrderDataType PARAM_NAME = new OrderDataType( "PARAM_NAME");
-//    public static final OrderDataType PARAM_VALUE = new OrderDataType( "PARAM_VALUE");
-
 // param name for subscriber name and sur name
     public static final String FIRST_NAME ="first_name";
     public static final String LAST_NAME ="last_name";
