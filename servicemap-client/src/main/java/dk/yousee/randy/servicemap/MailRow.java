a9ff9a6d73a32755406268768c01fb21886181ab
Anders Kristian Andersen <aka@myself.com>
2013-02-11 12:22:29 +0100

    TAYS-1408 product code is key, product name is a description

--- servicemap-client/src/main/java/dk/yousee/randy/servicemap/MailRow.java
+++ servicemap-client/src/main/java/dk/yousee/randy/servicemap/MailRow.java
@@ -14,8 +14,8 @@ public class MailRow {
 
     public MailRow(JsonObject node) {
         anlaeg=node.get("anlaeg").getAsString();
-        productName =node.get("product_name").getAsString();
         productCode =node.get("product_code").getAsString();
+        productName =node.get("product_name").getAsString();
         subPos=node.get("sub_pos").getAsString();
     }
 
@@ -24,19 +24,24 @@ public class MailRow {
     public String getAnlaeg() {
         return anlaeg;
     }
+    /**
+     * Key to foreningsmail
+     */
+    private String productCode;
 
+    public String getProductCode() {
+        return productCode;
+    }
+
+    /**
+     * Description of forenings mail
+     */
     private String productName;
 
     public String getProductName() {
         return productName;
     }
 
-
-    private String productCode;
-
-    public String getProductCode() {
-        return productCode;
-    }
 
     private String subPos;
 
@@ -48,8 +53,8 @@ public class MailRow {
     public String toString() {
         final StringBuilder sb = new StringBuilder();
         sb.append("{\"anlaeg\":\"").append(getAnlaeg()).append('"');
-        sb.append(", \"product_name\":\"").append(getProductName()).append('"');
         sb.append(", \"product_code\":\"").append(getProductCode()).append('"');
+        sb.append(", \"product_name\":\"").append(getProductName()).append('"');
         sb.append(", \"sub_pos\":\"").append(getSubPos()).append('"');
         sb.append('}');
         return sb.toString();
