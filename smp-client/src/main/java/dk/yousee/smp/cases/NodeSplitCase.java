7aa71e6193cefa516dea904b92432f7a57c424ea
jacoblorensen@gmail.com <>
2013-07-04 22:45:16 +0200

    Tays-1672 #comment Add modemmodel, vendor and serial number to nodesplit use case

--- smp-client/src/main/java/dk/yousee/smp/cases/NodeSplitCase.java
+++ smp-client/src/main/java/dk/yousee/smp/cases/NodeSplitCase.java
@@ -6,30 +6,33 @@ import dk.yousee.smp.order.model.Acct;
 import dk.yousee.smp.order.model.OrderService;
 
 /**
- * Created by IntelliJ IDEA.
- * User: m14857
- * Date: Dec 2, 2010
- * Time: 2:30:40 PM
- * The Node Split use case
+ * Created by IntelliJ IDEA. User: m14857 Date: Dec 2, 2010 Time: 2:30:40 PM The
+ * Node Split use case
  */
 public class NodeSplitCase extends AbstractCase {
-
     /**
      * Constructor with the customer account.
      *
-     * @param acct    account id
+     * @param acct account id
      * @param service service to sigma
      */
     public NodeSplitCase(Acct acct, OrderService service) {
         super(acct, service);
     }
 
-    public HsdAccess updateForNodeSplit(ModemId modemId, String gi_address, String cm_mac) {
+    public HsdAccess updateForNodeSplit(ModemId modemId, String gi_address, String cm_mac,
+            String modemModel,
+            String vendor,
+            String serial) {
         HsdAccess ha = getModel().alloc().HsdAccess(modemId);
         ha.cm_mac.setValue(cm_mac);
         ha.gi_address.setValue(gi_address);
+        if (modemModel != null)
+            ha.cm_model.setValue(modemModel);
+        if (vendor != null)
+            ha.cm_manufacturer.setValue(vendor);
+        if (serial != null)
+            ha.cm_serial_number.setValue(serial);
         return ha;
     }
-
-
 }
