d14e4e411f4770402a532e55b4c65c3bdf87e7cb
Jacob Lorensen <jalor@yousee.dk>
2013-12-14 13:50:57 +0100

    Remove comment about stack overflow

--- jsonlogging/src/main/java/dk/yousee/randy/logging/ValueFormatter.java
+++ jsonlogging/src/main/java/dk/yousee/randy/logging/ValueFormatter.java
@@ -13,15 +13,6 @@ package dk.yousee.randy.logging;
  * <li>Subscriber numbers
  * <li>&hellip;
  * </ul>
- * This formatter is not for structured data. Structured data would be present
- * in the http request or response entity, both of which are handled separately
- * and passed unchanged directly through to the log formatter, thus ultimately
- * being passed through the war project's standard object serialization process.
- * <p>
- * Specifically, changing the return type to Object&mdash;thus producing a
- * multivariate method&mdash;means the resulting json will not be acccepted into
- * Kibana/logstash<p>
- * Enquiries are proceeding as to what the actual formatting problem <i>is</i>
  *
  * @author Jacob Lorensen, TDC, December 2013
  */
