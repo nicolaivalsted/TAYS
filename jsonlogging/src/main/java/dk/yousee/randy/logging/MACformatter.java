f39c366eeff4330562d740abb15c01d7371e29a3
Jacob Lorensen <jalor@yousee.dk>
2014-06-19 11:12:47 +0200

    Remove @Component from MacaddressFormatter. NOt needed

--- jsonlogging/src/main/java/dk/yousee/randy/logging/MACformatter.java
+++ jsonlogging/src/main/java/dk/yousee/randy/logging/MACformatter.java
@@ -5,13 +5,11 @@
 package dk.yousee.randy.logging;
 
 import dk.yousee.randy.macaddress.MACaddress;
-import org.springframework.stereotype.Component;
 
 /**
  * Parse and re-format MAC addresses so they appear in a consistent format throughout the json log files.
  * @author Jacob Lorensen, TDC, December 2013.
  */
-@Component
 public class MACformatter implements ValueFormatter {
     @Override
     public String format(String arg) {
