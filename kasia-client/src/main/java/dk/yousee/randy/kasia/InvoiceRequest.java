c584878f47ed6de224c2abc8bde65a3aff6020f7
Per Olesen <per.olesen@gmail.com>
2014-08-11 11:49:03 +0200

    TAYS-2211: Extended bio invoicerequest to be able to buy stuff with a price (for BB)

--- kasia-client/src/main/java/dk/yousee/randy/kasia/InvoiceRequest.java
+++ kasia-client/src/main/java/dk/yousee/randy/kasia/InvoiceRequest.java
@@ -11,13 +11,23 @@ import com.google.gson.JsonObject;
  */
 public class InvoiceRequest {
 
+    public static final String SALES_ITEM_RENT_MOVIE = "2800001";
+    public static final String SALES_ITEM_BUY_KLIPPEKORT = "2800002";
+    public static final String SALES_ITEM_BUY_MOVIE = "2800003";
+
     private String customer;
     private String salesItem;
     private String title;
     private String user;
     private String system;
     private String vodId;
+    private String price;
+    private String klientFunktion;
+    private String salgskanal;
 
+    /**
+     * Old original constructor for YouBio invoices.
+     */
     public InvoiceRequest(String customer, String salesItem, String title, String user, String system, String vodId) {
         this.customer = customer;
         this.salesItem = salesItem;
@@ -25,40 +35,63 @@ public class InvoiceRequest {
         this.user = user;
         this.system = system;
         this.vodId = vodId;
+        this.klientFunktion = "rent-movie";
+        this.salgskanal = "K";
+    }
+
+    /**
+     * Constructor which takes all properties - to be used via static named constructors for better readability.
+     */
+    protected InvoiceRequest(String customer, String salesItem, String title, String user, String system, String vodId, String price, String klientFunktion, String salgskanal) {
+        this.customer = customer;
+        this.salesItem = salesItem;
+        this.title = title;
+        this.user = user;
+        this.system = system;
+        this.vodId = vodId;
+        this.price = price;
+        this.klientFunktion = klientFunktion;
+        this.salgskanal = salgskanal;
+    }
+
+    public static InvoiceRequest createMovieWithPrice(String customer, String salesItem, String title, String user, String system, String vodId, String price, String klientFunktion) {
+        return new InvoiceRequest(customer, salesItem, title, user, system, vodId, price, klientFunktion, "W");
+    }
+
+    public static InvoiceRequest createBuyKlippekortWithPrice(String customer, String salesItem, String user, String system, String price, String klientFunktion) {
+        // no "title" nor "vodk-id" for this..
+        return new InvoiceRequest(customer, salesItem, null, user, system, null, price, klientFunktion, "W");
     }
 
     public JsonObject printJson() {
-        String res;
-        res = String.format(
-            "{  \"kundeid\" : \"" + customer + "\",\n" +
-                "        \"handlinger\" : [{\n" +
-                "        \"handling\" : \"OPRET\",\n" +
-                "            \"varenr\" : \"" + salesItem + "\",\n" +
-                "            \"title\" : \"" + title + "\"\n" +
-                "    }],\n" +
-                "        \"info\" : {\n" +
-                "            \"salgskanal\" : \"K\",\n" +
-                "            \"klient-funktion\" : \"rent-movie\",\n" +
-                "            \"klient-bruger\" : \"" + user + "\",\n" +
-                "            \"klient-system\" : \"" + system + "\"\n" +
-                "    }\n" +
-                "}\n");
-        JsonObject order = new JsonObject();       
+        JsonObject order = new JsonObject();
         order.addProperty("kundeid", customer);
         
         JsonArray handlinger = new JsonArray();
         JsonObject handling = new JsonObject();
         handling.addProperty("handling", "OPRET");
         handling.addProperty("varenr", salesItem);
-        handling.addProperty("title", title);
-        handling.addProperty("vodk-id", vodId);
+
+        // "title" and "vodk-id" not present on buy-klippekort
+        if (title != null && !"".equals(title)) {
+            handling.addProperty("title", title);
+        }
+        if (vodId != null && !"".equals(vodId)) {
+            handling.addProperty("vodk-id", vodId);
+        }
+
+        // old bio orders did not include a price directly
+        if (price != null && !"".equals(price)) {
+            handling.addProperty("pris", price);
+        }
+
         handlinger.add(handling);
         
         order.add("handlinger", handlinger);
         
         JsonObject info = new JsonObject();
-        info.addProperty("salgskanal", "K");
-        info.addProperty("klient-funktion", "rent-movie");
+        info.addProperty("salgskanal", salgskanal);
+        info.addProperty("klient-funktion", klientFunktion);
         info.addProperty("klient-bruger", user);
         info.addProperty("klient-system", system);
         order.add("info", info);
---
+++ kasia-client/src/test/java/dk/yousee/randy/kasia/InvoiceRequestTest.java
@@ -0,0 +1,32 @@
+package dk.yousee.randy.kasia;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class InvoiceRequestTest {
+
+    @Test
+    public void createYouBioOrder() throws Exception {
+        String s = new InvoiceRequest("612345678", "salesItem", "title", "user", "system", "vodid").printJson().toString();
+        Assert.assertEquals("{\"kundeid\":\"612345678\",\"handlinger\":[{\"handling\":\"OPRET\",\"varenr\":\"salesItem\",\"title\":\"title\",\"vodk-id\":\"vodid\"}],\"info\":{\"salgskanal\":\"K\",\"klient-funktion\":\"rent-movie\",\"klient-bruger\":\"user\",\"klient-system\":\"system\"}}", s);
+    }
+
+    @Test
+    public void createRentMovieWithPrice() throws Exception {
+        String s = InvoiceRequest.createMovieWithPrice("612345678", InvoiceRequest.SALES_ITEM_RENT_MOVIE, "title", "user", "system", "vodid", "42.00", "rent-movie").printJson().toString();
+        Assert.assertEquals("{\"kundeid\":\"612345678\",\"handlinger\":[{\"handling\":\"OPRET\",\"varenr\":\"2800001\",\"title\":\"title\",\"vodk-id\":\"vodid\",\"pris\":\"42.00\"}],\"info\":{\"salgskanal\":\"W\",\"klient-funktion\":\"rent-movie\",\"klient-bruger\":\"user\",\"klient-system\":\"system\"}}", s);
+    }
+
+    @Test
+    public void createBuyMovieWithPrice() throws Exception {
+        String s = InvoiceRequest.createMovieWithPrice("612345678", InvoiceRequest.SALES_ITEM_BUY_MOVIE, "title", "user", "system", "vodid", "42.00", "buy-movie").printJson().toString();
+        Assert.assertEquals("{\"kundeid\":\"612345678\",\"handlinger\":[{\"handling\":\"OPRET\",\"varenr\":\"2800003\",\"title\":\"title\",\"vodk-id\":\"vodid\",\"pris\":\"42.00\"}],\"info\":{\"salgskanal\":\"W\",\"klient-funktion\":\"buy-movie\",\"klient-bruger\":\"user\",\"klient-system\":\"system\"}}", s);
+    }
+
+    @Test
+    public void createBuyKlippekortWithPrice() throws Exception {
+        String s = InvoiceRequest.createBuyKlippekortWithPrice("612345678", InvoiceRequest.SALES_ITEM_BUY_KLIPPEKORT, "user", "system", "42.00", "buy-klippekort").printJson().toString();
+        Assert.assertEquals("{\"kundeid\":\"612345678\",\"handlinger\":[{\"handling\":\"OPRET\",\"varenr\":\"2800002\",\"pris\":\"42.00\"}],\"info\":{\"salgskanal\":\"W\",\"klient-funktion\":\"buy-klippekort\",\"klient-bruger\":\"user\",\"klient-system\":\"system\"}}", s);
+    }
+
+}
