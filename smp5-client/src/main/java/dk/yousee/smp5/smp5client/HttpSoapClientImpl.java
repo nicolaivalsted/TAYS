660b947073b38ffcaa8a9fac425e2ca6f6f62b70
Tiago Carvalho <carvalho_1992@hotmail.com>
2015-12-23 12:10:39 +0000

    removed body encode

--- smp5-client/src/main/java/dk/yousee/smp5/smp5client/HttpSoapClientImpl.java
+++ smp5-client/src/main/java/dk/yousee/smp5/smp5client/HttpSoapClientImpl.java
@@ -63,7 +63,7 @@ class HttpSoapClientImpl extends AbstractClient<Smp5ConnectorImpl> implements Sm
 		postMethod.setRequestHeader("Authorization", getConnector().encodeBasic());
 
 		String body = String
-				.format("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+				.format(""
 						+ "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"
 						+ "<soapenv:Body>%s </soapenv:Body>  </soapenv:Envelope>", xmlRequest);
 
