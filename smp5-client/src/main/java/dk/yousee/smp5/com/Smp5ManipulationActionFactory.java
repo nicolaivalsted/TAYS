cf10639bc49fcfc7a8b359ca5d5606079bfc4364
Tiago Carvalho <carvalho_1992@hotmail.com>
2016-09-14 13:40:47 +0200

    Added missing actions

--- smp5-client/src/main/java/dk/yousee/smp5/com/Smp5ManipulationActionFactory.java
+++ smp5-client/src/main/java/dk/yousee/smp5/com/Smp5ManipulationActionFactory.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import dk.yousee.smp5.order.model.Smp5ManipulationAction;
+import dk.yousee.smp.order.model.SmpManipulationAction;
 import dk.yousee.smp5.order.model.Action;
 
 /**
@@ -16,9 +17,14 @@ public class Smp5ManipulationActionFactory {
 
 	static {
 		table.put(Action.ACTIVATE, Smp5ManipulationAction.ADD);
+		table.put(Action.DEACTIVATE, Smp5ManipulationAction.DELETE);
+		table.put(Action.RESUME, Smp5ManipulationAction.UPDATE);
 		table.put(Action.UPDATE, Smp5ManipulationAction.UPDATE);
-		table.put(Action.DELETE, Smp5ManipulationAction.DELETE);
+		table.put(Action.SUSPEND, Smp5ManipulationAction.UPDATE);
+	    table.put(Action.BLOCK, Smp5ManipulationAction.UPDATE);
 		table.put(Action.REPROV, Smp5ManipulationAction.REPROV);
+		table.put(Action.SWAP, Smp5ManipulationAction.UPDATE);
+        table.put(Action.DELETE, Smp5ManipulationAction.DELETE);
 	}
 
 	public Smp5ManipulationAction toAction(Action action) {
