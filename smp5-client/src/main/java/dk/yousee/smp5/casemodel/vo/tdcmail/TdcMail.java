4801d374bd21e7965c386ba9c7d2ef68650a784d
Tiago Carvalho <carvalho_1992@hotmail.com>
2016-09-17 20:07:06 +0200

    updated tdcmail

--- smp5-client/src/main/java/dk/yousee/smp5/casemodel/vo/tdcmail/TdcMail.java
+++ smp5-client/src/main/java/dk/yousee/smp5/casemodel/vo/tdcmail/TdcMail.java
@@ -32,7 +32,7 @@ public class TdcMail extends BasicUnit {
 		return (TdcMailService) super.getParent();
 	}
 
-	protected PropHolder sik = new PropHolder(this, "sik");
+	public PropHolder sik = new PropHolder(this, "sik");
 	public PropHolder yspro_pcode = new PropHolder(this, "yspro_pcode", true);
     public PropHolder kpm_number = new PropHolder(this, "kpm_number", true);
     public PropHolder yspro_provisioning_id = new PropHolder(this, "yspro_provisioningid");
