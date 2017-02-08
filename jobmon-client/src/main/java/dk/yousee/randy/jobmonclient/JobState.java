c2ef29730c7412c788bb31b851ec58e9af503c97
Torben Holmbæck <holmbaeck@gmail.com>
2017-02-08 16:25:07 +0100

    TAYS-2705 - Missing Icinga alert for Failed jobs

--- jobmon-client/src/main/java/dk/yousee/randy/jobmonclient/JobState.java
+++ jobmon-client/src/main/java/dk/yousee/randy/jobmonclient/JobState.java
@@ -5,7 +5,7 @@ package dk.yousee.randy.jobmonclient;
  * @author m27236
  */
 public enum JobState {
-    STARTED("STARTED"), RUNNING("RUNNING"), FAIL("FAIL"), FAIL_DEPENDENCY("FAIL-DEPENDENCY"), DONE("DONE");
+    STARTED("STARTED"), RUNNING("RUNNING"), FAIL("FAIL"), FAIL_DEPENDENCY("FAIL-DEPENDENCY"), FAIL_HANDLED("FAIL_HANDLED"), DONE("DONE");
     private String str;
     
     private JobState(String str){
